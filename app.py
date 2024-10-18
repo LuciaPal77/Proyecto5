03944ddf41eb5f492bfc2834d3cc66c091e149d1
LuciaPal77 <lucia.luli.77@gmail.com>
2024-10-18 18:34:15 -0300

    g

--- app.py
+++ app.py
@@ -1,29 +1,28 @@
-import pandas
-import plotly.express
-import streamlit 
+import pandas as pd
+import plotly.express as px
+import streamlit as st
 
-streamlit.header('Estudio de Vehiculos')
+st.header('Estudio de Vehiculos')
 
-car_data = pandas.read_csv('vehicles_us.csv') # leer los datos
-hist_button = streamlit.button('Construir histograma') # crear un botón
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+#Boton Histograma
+hist_button = st.button('Construir histograma')
         
 if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-    streamlit.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             # crear un histograma
-    fig = plotly.express.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-    streamlit.plotly_chart(fig, use_container_width=True)
+    fig = px.histogram(car_data, x="odometer")
+        #Mostramos el histograma
+    st.plotly_chart(fig, use_container_width=True)
 
-build_histogram = streamlit.checkbox('Construir un histograma')
+build_histogram = st.checkbox('Construir un histograma')
 if build_histogram: # si la casilla de verificación está seleccionada
-    streamlit.write('Construir un histograma para la columna odómetro')
+    st.write('Construir un histograma para la columna odómetro')
 
 
-scatt_button = streamlit.button('Construir gráfica de dispersión')
+scatt_button = st.button('Construir gráfica de dispersión')
 if scatt_button:
-    streamlit.write('Crea un gráfico de dispersión sobre la columna precio')
-    fig2 = plotly.express.scatter(car_data, x="price")
-streamlit.scatter_chart(car_data)  #Grafica de dispersion
+    st.write('Crea un gráfico de dispersión sobre la columna precio')
+    fig2 = px.scatter(car_data, x="price")
+st.plotly_chart(car_data)  #Grafica de dispersion
