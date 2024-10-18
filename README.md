9aa6c6474a242d43d791f657f987f4f4c3ff6b3e
LuciaPal77 <lucia.luli.77@gmail.com>
2024-10-18 18:24:58 -0300

    f

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Proyecto5
+En este proyecto creamos una api web
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pandas
 import plotly.express
-import streamlit
-        
+import streamlit 
+
+streamlit.header('Estudio de Vehiculos')
+
 car_data = pandas.read_csv('vehicles_us.csv') # leer los datos
 hist_button = streamlit.button('Construir histograma') # crear un botón
         
@@ -14,3 +16,14 @@ if hist_button: # al hacer clic en el botón
         
             # mostrar un gráfico Plotly interactivo
     streamlit.plotly_chart(fig, use_container_width=True)
+
+build_histogram = streamlit.checkbox('Construir un histograma')
+if build_histogram: # si la casilla de verificación está seleccionada
+    streamlit.write('Construir un histograma para la columna odómetro')
+
+
+scatt_button = streamlit.button('Construir gráfica de dispersión')
+if scatt_button:
+    streamlit.write('Crea un gráfico de dispersión sobre la columna precio')
+    fig2 = plotly.express.scatter(car_data, x="price")
+streamlit.scatter_chart(car_data)  #Grafica de dispersion
